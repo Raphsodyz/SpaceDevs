8bea7e98526b3a2e68bea2b75fd9fa6e46fa02aa
Raphsodyz <arthurvsr@gmail.com>
2023-12-30 14:43:03 -0300

    Changed interface to use views

--- Business/Interface/ILaunchApiBusiness.cs
+++ Business/Interface/ILaunchApiBusiness.cs
@@ -2,12 +2,13 @@
 using Data.Interface;
 using Domain.Entities;
 using Cross.Cutting.Helper;
+using Data.Materializated.Views;
 
 namespace Business.Interface
 {
     public interface ILaunchApiBusiness : IBusinessBase<Launch, ILaunchRepository>
     {
-        Task<LaunchDTO> GetOneLaunch(Guid? launchId);
+        Task<LaunchView> GetOneLaunch(Guid? launchId);
         Task<Pagination<LaunchDTO>> GetAllLaunchPaged(int? page);
         Task SoftDeleteLaunch(Guid? launchId);
         Task<LaunchDTO> UpdateLaunch(Guid? launchId);
