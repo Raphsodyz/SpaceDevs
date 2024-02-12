54791e7f8645a427ed95f995cc1e6daf7b551b10
Arthur Vinícius <arthurvsr@gmail.com>
2024-02-11 23:09:18 -0300

    Fix(Request): Fix on updateLaunchRequest obj error message

--- Business/DTO/Request/UpdateLaunchRequest.cs
+++ Business/DTO/Request/UpdateLaunchRequest.cs
@@ -8,7 +8,7 @@ namespace Business.DTO.Request
 {
     public class UpdateLaunchRequest
     {
-        [Range(0, 100, ErrorMessage = "The value must be greater than 0 and less 100.")]
+        [Range(0, 100, ErrorMessage = "The value on the field {0} must be greater than 0 and less 100.")]
         [Display(Name = "Limit")]
         public int? Limit { get; set; }
 
@@ -16,7 +16,7 @@ namespace Business.DTO.Request
         [Display(Name = "Skip")]
         public int? Skip { get; set; }
 
-        [Range(0, 15, ErrorMessage = "The value must be greater than 0 and less 15.")]
+        [Range(0, 15, ErrorMessage = "The value on the field {0} must be greater than 0 and less 15.")]
         [Display(Name = "Iterations")]
         public int? Iterations { get; set; }
     }
