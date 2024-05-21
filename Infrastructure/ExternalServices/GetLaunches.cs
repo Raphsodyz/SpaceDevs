1394c9e8d27a2c5c0d7f01ef59bdf08ebc949a49
Arthur Vinícius <arthurvsr@gmail.com>
2024-05-20 23:32:35 -0300

    Feat(Infrastructure): Implementation of IRequestLaunchService

--- Infrastructure/ExternalServices/GetLaunches.cs
+++ Infrastructure/ExternalServices/GetLaunches.cs
@@ -1,4 +1,6 @@
 using System.Net.Http.Json;
+using System.Text.Json;
+using AutoMapper;
 using Cross.Cutting.Helper;
 using Domain.Entities;
 using Domain.ExternalServices;
@@ -9,9 +11,11 @@ namespace Infrastructure.ExternalServices
     public class GetLaunches : IRequestLaunchService
     {
         private readonly IHttpClientFactory _client;
-        public GetLaunches(IHttpClientFactory client)
+        private readonly IMapper _mapper;
+        public GetLaunches(IHttpClientFactory client, IMapper mapper)
         {
             _client = client;
+            _mapper = mapper;
         }
 
         public async Task<List<Launch>> RequestLaunchSet(int limit, int offset)
@@ -25,18 +29,41 @@ namespace Infrastructure.ExternalServices
                     throw new HttpRequestException($"{response.StatusCode} - {ErrorMessages.LaunchApiEndPointError}");
 
                 RequestLaunchDTO dataList = await response.Content.ReadFromJsonAsync<RequestLaunchDTO>() ?? throw new HttpRequestException(ErrorMessages.DeserializingContentError);
-                if ((bool)!dataList.Results?.Any())
+                if (!dataList.Results.Any())
                     throw new KeyNotFoundException(ErrorMessages.NoDataFromSpaceDevApi);
+
+                var launches = _mapper.Map<List<Launch>>(dataList.Results);
+                return launches;
             }
             catch(Exception ex)
             {
-
+                //Needs implementate the exception handling for generate logs;
+                throw new NotImplementedException();
             }
         }
 
-        public Task<Launch> RequestLaunchById(Guid id)
+        public async Task<Launch> RequestLaunchById(Guid id)
         {
-            throw new NotImplementedException();
+            using var client = _client.CreateClient();
+            try
+            {
+                string url = $"{EndPoints.TheSpaceDevsLaunchEndPoint}{id}";
+                HttpResponseMessage response = await client.GetAsync(url);
+                if (!response.IsSuccessStatusCode)
+                    throw new HttpRequestException($"{response.StatusCode} - {ErrorMessages.LaunchApiEndPointError}");
+                
+                var updatedLaunch = await response.Content.ReadFromJsonAsync<LaunchDTO>();
+                if(ObjectHelper.IsObjectEmpty(updatedLaunch))
+                    throw new JsonException(ErrorMessages.DeserializingContentError);
+
+                var launch = _mapper.Map<Launch>(updatedLaunch);
+                return launch;
+            }
+            catch(Exception ex)
+            {
+                //Needs implementate the exception handling for generate logs;
+                throw new NotImplementedException();
+            }
         }
     }
 }
