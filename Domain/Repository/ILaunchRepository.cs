78d400c18131a1ffacce7fc0863262adadf04e72
Arthur Vinícius <arthurvsr@gmail.com>
2024-05-28 21:50:55 -0300

    Feat(Infrastructure): Base data access methods for update method on launch repository

--- Domain/Repository/ILaunchRepository.cs
+++ Domain/Repository/ILaunchRepository.cs
@@ -6,6 +6,7 @@ namespace Domain.Interface
     public interface ILaunchRepository : IGenericRepository<Launch>
     {
         Task<IEnumerable<TResult>> ILikeSearch<TResult>(string searchTerm, Expression<Func<Launch, TResult>> selectColumns, string includedProperties = null);
-        Task SetUpBaseEntityData(Launch launch);
+        Task SetUpBaseEntityDTO(Launch launch);
+        Task SaveOnUpdateLaunch(Launch launch, Guid? idStatus, Guid? idLaunchServiceProvider, Guid? idRocket, Guid? idMission, Guid? idPad);
     }
 }
--- Infrastructure/Persistence/Repository/LaunchRepository.cs
+++ Infrastructure/Persistence/Repository/LaunchRepository.cs
@@ -1,4 +1,5 @@
-﻿using Domain.Entities;
+﻿using AutoMapper;
+using Domain.Entities;
 using Domain.Interface;
 using Infrastructure.DTO;
 using Infrastructure.Persistence.Context;
@@ -9,9 +10,10 @@ namespace Infrastructure.Persistence.Repository
 {
     public class LaunchRepository : GenericRepository<Launch>, ILaunchRepository
     {
-        public LaunchRepository(FutureSpaceContext context):base(context)
+        private readonly IMapper _mapper;
+        public LaunchRepository(FutureSpaceContext context, IMapper mapper):base(context)
         {
-            
+            _mapper = mapper;
         }
 
         public async Task<IEnumerable<TResult>> ILikeSearch<TResult>(string searchTerm, Expression<Func<Launch, TResult>> selectColumns, string includedProperties = null)
@@ -31,7 +33,7 @@ namespace Infrastructure.Persistence.Repository
             return result;
         }
 
-        public async Task SetUpBaseEntityData(Launch launch)
+        public async Task SetUpBaseEntityDTO(Launch launch)
         {
             var launchBaseEntityCompoundData = await GetAllSelectedColumns(
                 filters: new List<Expression<Func<Launch, bool>>>() { l => l.ApiGuid == launch.ApiGuid },
@@ -40,14 +42,18 @@ namespace Infrastructure.Persistence.Repository
                 { 
                     LaunchId = l.Id, LaunchApiGuid = l.ApiGuid, LaunchAtualizationDate = l.AtualizationDate, LaunchImportedT = l.ImportedT, LaunchStatus = l.EntityStatus,
                     IdStatus = l.IdStatus, StatusId = l.Status.Id, StatusIdFromApi = l.Status.IdFromApi, StatusAtualizationDate = l.Status.AtualizationDate, StatusImportedT = l.Status.ImportedT, StatusStatus = l.Status.EntityStatus,
-                    IdLaunchServiceProvider = l.IdLaunchServiceProvider, LaunchServiceProviderId = l.IdLaunchServiceProvider, LaunchServiceProviderIdFromApi = l.LaunchServiceProvider.IdFromApi, LaunchServiceProviderAtualizationDate = l.LaunchServiceProvider.AtualizationDate,
+                    IdLaunchServiceProvider = l.IdLaunchServiceProvider, LaunchServiceProviderId = l.LaunchServiceProvider.Id, LaunchServiceProviderIdFromApi = l.LaunchServiceProvider.IdFromApi, LaunchServiceProviderAtualizationDate = l.LaunchServiceProvider.AtualizationDate,
                     LaunchServiceProviderImportedT = l.LaunchServiceProvider.ImportedT, LaunchServiceProviderStatus = l.LaunchServiceProvider.EntityStatus,
-                    IdRocket = l.IdRocket, RocketId = l.Rocket.Id, RocketIdFromApi = l.Rocket.IdFromApi, RocketAtualizationDate = l.Rocket.AtualizationDate, RocketImportedT = l.Rocket.ImportedT, RocketStatus = l.Rocket.EntityStatus
+                    IdRocket = l.IdRocket, RocketId = l.Rocket.Id, RocketIdFromApi = l.Rocket.IdFromApi, RocketAtualizationDate = l.Rocket.AtualizationDate, RocketImportedT = l.Rocket.ImportedT, RocketStatus = l.Rocket.EntityStatus,
                     IdConfiguration = l.Rocket.IdConfiguration, ConfigurationId = l.Rocket.Configuration.Id, ConfigurationIdFromApi = l.Rocket.Configuration.IdFromApi, ConfigurationAtualizationDate = l.Rocket.Configuration.AtualizationDate, ConfigurationImportedT = l.Rocket.Configuration.ImportedT, ConfigurationStatus = l.Rocket.Configuration.EntityStatus,
+                    IdMission = l.IdMission, MissionId = l.Mission.Id, MissionIdFromApi = l.Mission.IdFromApi, MissionAtualizationDate = l.Mission.AtualizationDate, MissionImportedT = l.Mission.ImportedT, MissionStatus = l.Mission.EntityStatus,
+                    IdOrbit = l.Mission.IdOrbit, OrbitId = l.Mission.Orbit.Id, OrbitIdFromApi = l.Mission.Orbit.IdFromApi, OrbitAtualizationDate = l.Mission.Orbit.AtualizationDate, OrbitImportedT = l.Mission.Orbit.ImportedT, OrbitStatus = l.Mission.Orbit.EntityStatus,
+                    IdPad = l.IdPad, PadId = l.Pad.Id, PadIdFromApi = l.Pad.IdFromApi, PadAtualizationDate = l.Pad.AtualizationDate, PadImportedT = l.Pad.ImportedT, PadStatus = l.Pad.EntityStatus,
+                    IdLocation = l.Pad.IdLocation, LocationId = l.Pad.Location.Id, LocationIdFromApi = l.Pad.Location.IdFromApi, LocationAtualizationDate = l.Pad.Location.AtualizationDate, LocationImportedT = l.Pad.Location.ImportedT, LocationStatus = l.Pad.Location.EntityStatus
                 },
-                buildObject: l => new LaunchBaseEntityCompoundData()
+                buildObject: l => new LaunchBaseEntityCompoundDTO()
                 {
-                    LaunchBaseEntity = new BaseEntityLaunchData()
+                    LaunchBaseEntity = new BaseEntityLaunchDTO()
                     {
                         Id = l.LaunchId,
                         ApiGuid = l.LaunchApiGuid,
@@ -55,7 +61,7 @@ namespace Infrastructure.Persistence.Repository
                         ImportedT = l.LaunchImportedT,
                         Status = l.LaunchStatus
                     },
-                    StatusBaseEntity = l.IdStatus != null ? new BaseEntityData()
+                    StatusBaseEntity = l.IdStatus != null ? new BaseEntityDTO()
                     {
                         Id = l.StatusId,
                         IdFromApi = l.StatusIdFromApi,
@@ -63,7 +69,7 @@ namespace Infrastructure.Persistence.Repository
                         ImportedT = l.StatusImportedT,
                         Status = l.StatusStatus
                     } : null,
-                    LaunchServiceProviderBaseEntity = l.IdLaunchServiceProvider != null ? new BaseEntityData()
+                    LaunchServiceProviderBaseEntity = l.IdLaunchServiceProvider != null ? new BaseEntityDTO()
                     {
                         Id = l.LaunchServiceProviderId,
                         IdFromApi = l.LaunchServiceProviderIdFromApi,
@@ -71,7 +77,7 @@ namespace Infrastructure.Persistence.Repository
                         ImportedT = l.LaunchServiceProviderImportedT,
                         Status = l.LaunchServiceProviderStatus
                     } : null,
-                    RocketBaseEntity = l.IdRocket != null ? new BaseEntityData()
+                    RocketBaseEntity = l.IdRocket != null ? new BaseEntityDTO()
                     {
                         Id = l.RocketId,
                         IdFromApi = l.RocketIdFromApi,
@@ -79,16 +85,78 @@ namespace Infrastructure.Persistence.Repository
                         ImportedT = l.RocketImportedT,
                         Status = l.RocketStatus
                     } : null,
-                    ConfigurationBaseEntity = l.IdConfiguration != null ? new BaseEntityData()
+                    ConfigurationBaseEntity = l.IdConfiguration != null ? new BaseEntityDTO()
                     {
                         Id = l.ConfigurationId,
                         IdFromApi = l.ConfigurationIdFromApi,
                         AtualizationDate = l.ConfigurationAtualizationDate,
                         ImportedT = l.ConfigurationImportedT,
                         Status = l.ConfigurationStatus
+                    } : null,
+                    MissionBaseEntity = l.IdMission != null ? new BaseEntityDTO()
+                    {
+                        Id = l.MissionId,
+                        IdFromApi = l.MissionIdFromApi,
+                        AtualizationDate = l.MissionAtualizationDate,
+                        ImportedT = l.MissionImportedT,
+                        Status = l.MissionStatus
+                    } : null,
+                    OrbitBaseEntity = l.IdOrbit != null ? new BaseEntityDTO()
+                    {
+                        Id = l.OrbitId,
+                        IdFromApi = l.OrbitIdFromApi,
+                        AtualizationDate = l.OrbitAtualizationDate,
+                        ImportedT = l.OrbitImportedT,
+                        Status = l.OrbitStatus
+                    } : null,
+                    PadBaseEntity = l.IdPad != null ? new BaseEntityDTO()
+                    {
+                        Id = l.PadId,
+                        IdFromApi = l.PadIdFromApi,
+                        AtualizationDate = l.PadAtualizationDate,
+                        ImportedT = l.PadImportedT,
+                        Status = l.PadStatus
+                    } : null,
+                    LocationBaseEntity = l.IdLocation != null ? new BaseEntityDTO()
+                    {
+                        Id = l.LocationId,
+                        IdFromApi = l.LocationIdFromApi,
+                        AtualizationDate = l.LocationAtualizationDate,
+                        ImportedT = l.LocationImportedT,
+                        Status = l.LocationStatus
                     } : null
                 }
             );
+
+            _mapper.Map(launchBaseEntityCompoundData, launch);
+        }
+
+        public async Task SaveOnUpdateLaunch(Launch launch, Guid? idStatus, Guid? idLaunchServiceProvider, Guid? idRocket, Guid? idMission, Guid? idPad)
+        {
+            launch.IdStatus = idStatus;
+            launch.IdLaunchServiceProvider = idLaunchServiceProvider;
+            launch.IdRocket = idRocket;
+            launch.IdMission = idMission;
+            launch.IdPad = idPad;
+
+            var entry = _context.Entry(launch);
+            
+            if (entry.Reference(e => e.Status).IsLoaded)
+                entry.Reference(e => e.Status).IsLoaded = false;
+
+            if (entry.Reference(e => e.LaunchServiceProvider).IsLoaded)
+                entry.Reference(e => e.LaunchServiceProvider).IsLoaded = false;
+
+            if (entry.Reference(e => e.Rocket).IsLoaded)
+                entry.Reference(e => e.Rocket).IsLoaded = false;
+
+            if (entry.Reference(e => e.Mission).IsLoaded)
+                entry.Reference(e => e.Mission).IsLoaded = false;
+
+            if (entry.Reference(e => e.Pad).IsLoaded)
+                entry.Reference(e => e.Pad).IsLoaded = false;
+
+            await Save(launch);
         }
     }
 }
