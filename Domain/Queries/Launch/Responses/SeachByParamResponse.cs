5c5fb36f5c0de7855cd226b4d29457a09675e623
Arthur Vinícius <arthurvsr@gmail.com>
2024-05-30 13:45:39 -0300

    Feat(Domain): Request classes for update data set and search by param

---
+++ Domain/Queries/Launch/Responses/SeachByParamResponse.cs
@@ -0,0 +1,16 @@
+using Cross.Cutting.Helper;
+using Domain.Materializated.Views;
+using Domain.Shared;
+
+namespace Domain.Queries.Launch.Responses
+{
+    public class SeachByParamResponse : BaseResponse<Pagination<LaunchView>>
+    {
+        public SeachByParamResponse(bool success, string error, Pagination<LaunchView> data)
+        {
+            Success = success;
+            Error = error;
+            Data = data;
+        }
+    }
+}
