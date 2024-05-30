898cb0f412ea6cd9744da2f0936885af0dc55247
Arthur Vinícius <arthurvsr@gmail.com>
2024-05-30 13:44:25 -0300

    Feat(Application): Handler implementation for Search launch and update data set

---
+++ Application/Handlers/CommandHandlers/LaunchApi/UpdateDataSetHandler.cs
@@ -0,0 +1,71 @@
+using Application.Shared.Handler;
+using Application.Wrappers;
+using Cross.Cutting.Helper;
+using Domain.Commands.Launch.Requests;
+using Domain.Commands.Launch.Responses;
+using Domain.ExternalServices;
+using Domain.Handlers;
+using Domain.Interface;
+using MediatR;
+
+namespace Application.Handlers.CommandHandlers.LaunchApi
+{
+    public class UpdateDataSetHandler : BaseUpdateDataHandler, IRequestHandler<MediatrRequestWrapper<UpdateLaunchRequest, UpdateDataSetResponse>, UpdateDataSetResponse>, IUpdateDataSetHandler
+    {
+        private readonly IRequestLaunchService _request;
+        private readonly ILaunchViewRepository _launchViewRepository;
+        public UpdateDataSetHandler(
+            ILaunchRepository launchRepository,
+            IGenericDapperRepository genericDapperRepository,
+            IRequestLaunchService request,
+            ILaunchViewRepository launchViewRepository,
+            IUpdateLogRepository updateLogRepository)
+            : base(launchRepository, genericDapperRepository, updateLogRepository)
+        {
+            _request = request;
+            _launchViewRepository = launchViewRepository;
+        }
+
+        public async Task<UpdateDataSetResponse> Handle(MediatrRequestWrapper<UpdateLaunchRequest, UpdateDataSetResponse> request, CancellationToken cancellationToken)
+        {
+            var domainRequest = request.DomainRequest;
+            return await Handle(domainRequest, cancellationToken);
+        }
+
+        public async Task<UpdateDataSetResponse> Handle(UpdateLaunchRequest request, CancellationToken cancellationToken)
+        {
+            request.Limit ??= 100;
+            request.Iterations ??= 15;
+            int offset = request.Skip ??= 0, entityCounter = 0, max = offset + ((int)request.Iterations * (int)request.Limit);
+
+            try
+            {
+                for(int i = offset; i < max; i += (int)request.Limit)
+                {
+                    var launches = await _request.RequestLaunchSet((int)request.Limit, offset, entityCounter);
+                    foreach(var data in launches)
+                    {
+                        await SaveLaunch(data, request.ReplaceData ?? false);
+                        entityCounter++;
+                    }
+                }
+
+                await GenerateLog(offset, SuccessMessages.PartialImportSuccess, entityCounter, true);
+                entityCounter = 0;
+                offset += (int)request.Limit;
+            }
+            catch(Exception ex)
+            {
+                await GenerateLog(offset, ex.Message, entityCounter, false);
+                return new UpdateDataSetResponse(false, ex.Message, null);
+            }
+            finally
+            {
+                await _launchViewRepository.RefreshView();
+            }
+
+            await GenerateLog(offset, SuccessMessages.ImportedDataSuccess, entityCounter, true);
+            return new UpdateDataSetResponse(true, string.Empty, true);
+        }
+    }
+}
